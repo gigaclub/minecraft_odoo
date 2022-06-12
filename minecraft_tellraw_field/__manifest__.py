d7067edf7d67923a6fe533932543192b31f4abbb
Kevin <kevin@fritsch.vip>
2022-06-12 10:36:40 +0200

    added new module

---
+++ minecraft_tellraw_field/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Minecraft Tellraw Field",
+    "version": "14.0.1.0.0",
+    "category": "Minecraft",
+    "author": "GigaClub.net",
+    "website": "https://GigaClub.net/",
+    "license": "AGPL-3",
+    "depends": ["web"],
+    "data": [],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
