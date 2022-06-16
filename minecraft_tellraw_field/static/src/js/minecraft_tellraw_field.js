ad16a128d28cde16698a271e4f8b2e6ef24ffaf1
Kevin <kevin@fritsch.vip>
2022-06-16 16:31:45 +0200

    wip

--- minecraft_tellraw_field/static/src/js/minecraft_tellraw_field.js
+++ minecraft_tellraw_field/static/src/js/minecraft_tellraw_field.js
@@ -10,6 +10,8 @@ odoo.define("minecraft_tellraw_field.minecraft_tellraw_field", function (require
   const {Component} = owl;
   const {useState, useRef} = owl.hooks;
 
+  const values = [""];
+
   class MinecraftTellrawDialog extends Component {
     constructor(...args) {
       super(...args);
@@ -18,6 +20,58 @@ odoo.define("minecraft_tellraw_field.minecraft_tellraw_field", function (require
         clickEvent: false,
         hoverEvent: false,
       });
+      console.log(this._dialogRef);
+    }
+    buildValues(event) {
+      const $element = $(event.target.parentElement.parentElement);
+      const value = {};
+      const text = $element.find("#text").val();
+      if (text) {
+        value.text = text;
+      }
+      const color = $element.find("#color").val();
+      if (color) {
+        value.color = color;
+      }
+      const bold = $element.find("#bold").prop("checked");
+      if (bold) {
+        value.bold = true;
+      }
+      const italic = $element.find("#italic").prop("checked");
+      if (italic) {
+        value.italic = true;
+      }
+      const underlined = $element.find("#underlined").prop("checked");
+      if (underlined) {
+        value.underlined = true;
+      }
+      const strikethrough = $element.find("#strikethrough").prop("checked");
+      if (strikethrough) {
+        value.strikethrough = true;
+      }
+      const obfuscated = $element.find("#obfuscated").prop("checked");
+      if (obfuscated) {
+        value.obfuscated = true;
+      }
+      const font = $element.find("#font").val();
+      if (font) {
+        value.font = font;
+      }
+      const clickEvent = $element.find("#click-event").val();
+      if (clickEvent && clickEvent !== "none") {
+        value.clickEvent = {
+          action: clickEvent,
+          value: $element.find(`[name=${clickEvent}]`).val(),
+        };
+      }
+      const hoverEvent = $element.find("#hover-event").val();
+      if (hoverEvent && hoverEvent !== "none") {
+        value.hoverEvent = {
+          action: hoverEvent,
+          value: $element.find(`[name=${hoverEvent}]`).val(),
+        };
+      }
+      values.push(value);
     }
     onChangeClickEvent(event) {
       const value = event.target.value;
@@ -35,11 +89,21 @@ odoo.define("minecraft_tellraw_field.minecraft_tellraw_field", function (require
         this.state.hoverEvent = false;
       }
     }
-    onClickSave() {
+    onClickSave(event) {
+      this.buildValues(event);
       this._dialogRef.comp._close();
     }
     onClickCancel() {
       this._dialogRef.comp._close();
+    }
+    onClickOpenDropdown(event) {
+      const $element = $(event.target);
+      console.log($element.attr("aria-expanded"));
+      if ($element.attr("aria-expanded") === "true") {
+        $element.dropdown("hide");
+      } else {
+        $element.dropdown("toggle");
+      }
     }
   }
 
@@ -62,10 +126,15 @@ odoo.define("minecraft_tellraw_field.minecraft_tellraw_field", function (require
       this.state.minecraftTellrawTextDialog = true;
     }
     openLineBreak() {
-      console.log("openLineBreak");
+      values.push("\n");
+      console.log(values);
     }
     onDialogClosedModerationDiscard() {
+      console.log(values);
       this.state.minecraftTellrawTextDialog = false;
+    }
+    onClickOpenDropdown() {
+      console.log("test");
     }
   }
 
