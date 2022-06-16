106ad67a6647342ce8c6b7dceed2c0d9f20d2329
Kevin <kevin@fritsch.vip>
2022-06-16 13:12:34 +0200

    rewritten in OWL

--- minecraft_tellraw_field/static/src/js/field_registry.js
+++ minecraft_tellraw_field/static/src/js/field_registry.js
@@ -3,7 +3,7 @@ odoo.define("minecraft_tellraw_field.field_registry", function (require) {
 
   const MinecraftTellrawField = require("minecraft_tellraw_field.minecraft_tellraw_field");
 
-  const registry = require("web.field_registry");
+  const registry = require("web.field_registry_owl");
 
   registry.add("minecraft_tellraw_field", MinecraftTellrawField.MinecraftTellrawField);
 });
--- minecraft_tellraw_field/static/src/js/minecraft_tellraw_field.js
+++ minecraft_tellraw_field/static/src/js/minecraft_tellraw_field.js
@@ -1,58 +1,55 @@
 odoo.define("minecraft_tellraw_field.minecraft_tellraw_field", function (require) {
   "use strict";
 
-  const AbstractField = require("web.AbstractField");
-  const Dialog = require("web.Dialog");
+  const AbstractFieldOwl = require("web.AbstractFieldOwl");
   const core = require("web.core");
 
   const _lt = core._lt;
-  const QWeb = core.qweb;
 
-  const MinecraftTellrawDialog = Dialog.extend({
-    init: function (parent, options) {
-      this.options = options || {};
-      this._super(
-        parent,
-        _.extend(
-          {
-            size: "large",
-            title: _lt("Text"),
-            $content: $(QWeb.render("FieldMinecraftTellrawText")),
-            buttons: [
-              {
-                text: _lt("Save"),
-                classes: "btn-primary",
-                click: function () {
-                  console.log("test");
-                },
-                close: true,
-              },
-              {
-                text: _lt("Cancel"),
-                close: true,
-              },
-            ],
-          },
-          this.options
-        )
-      );
-    },
+  const {Component} = owl;
+  const {useState, useRef} = owl.hooks;
+
+  const components = {
+    Dialog: require("web.OwlDialog"),
+  };
+
+  class MinecraftTellrawDialog extends Component {
+    constructor(...args) {
+      super(...args);
+      this._dialogRef = useRef("dialog");
+      console.log(this._dialogRef);
+    }
+  }
+
+  Object.assign(MinecraftTellrawDialog, {
+    components,
+    template: "FieldMinecraftTellrawText",
   });
 
-  const MinecraftTellrawField = AbstractField.extend({
-    description: _lt("Minecraft Tellraw Field"),
-    supportedFieldTypes: ["serialized"],
-    template: "FieldMinecraftTellraw",
-    events: _.extend({}, AbstractField.prototype.events, {
-      "click button[name='text']": "openText",
-      "click button[name='lineBreak']": "openLineBreak",
-    }),
-    openText: function () {
-      new MinecraftTellrawDialog(this, {}).open();
-    },
-    openLineBreak: function () {
+  class MinecraftTellrawField extends AbstractFieldOwl {
+    constructor(...args) {
+      super(...args);
+      this.state = useState({
+        minecraftTellrawTextDialog: false,
+      });
+    }
+    openText(event) {
+      console.log("openText");
+      event.preventDefault();
+      this.state.minecraftTellrawTextDialog = true;
+    }
+    openLineBreak() {
       console.log("openLineBreak");
+    }
+  }
+
+  Object.assign(MinecraftTellrawField, {
+    components: {
+      MinecraftTellrawDialog,
     },
+    template: "FieldMinecraftTellraw",
+    supportedFieldTypes: ["serialized"],
+    description: _lt("Minecraft Tellraw Field"),
   });
 
   return {
